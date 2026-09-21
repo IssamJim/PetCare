5bc105e9fdb872b8599d2ade1451eb5ce9d37686
IssamJim <issamjimenez.98@gmail.com>
2026-09-20 23:58:47 -0700

    creamos una vista para el login y la agregamos a las urls del proyecto.

--- PetCare/urls.py
+++ PetCare/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from proyecto import views as views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.vista_login, name='login'),
 ]
--- proyecto/urls.py
+++ proyecto/urls.py
@@ -1,5 +1,5 @@
+from django.urls import path
 from django.shortcuts import render
 
-# Create your views here.
 def vista_login(request):
     return render(request, 'Login/index.html')
