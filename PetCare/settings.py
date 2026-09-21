a8a27fdde1ab1cff119e9759cb4246770244c0d8
IssamJim <issamjimenez.98@gmail.com>
2026-09-20 17:17:18 -0700

    Conectamos la base de datos MySQL con Django y añadimos la aplicación 'proyecto' a la lista de aplicaciones instaladas en el archivo settings.py.

--- PetCare/settings.py
+++ PetCare/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'proyecto',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'PetCare.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'petcaredb',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
